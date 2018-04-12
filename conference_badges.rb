5895fa7796494e1de4507c373985f37f664c6bae
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-12 17:52:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  name_ray = []
+  array.each do |name|
+    name_ray.push(badge_maker(name))
+  end
+  return name_ray
+end
+
+def assign_rooms(array)
+  array.each.with_index(1) do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+end
+
+def printer()
+  puts batch_badge_creator()
+  puts assign_rooms()
+end
